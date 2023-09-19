d56feaa273066a4c4296fe7e4e16ef6aac9e0f21
MaximusYakuza <mmoralesalta@gmail.com>
2023-09-18 22:45:26 -0600

    Cambios

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var app = express();
 const PUERTO = 80;
 
 app.listen(PUERTO, function(){
-    console.log('Servidor http corriendo en el puerto 80');
+    console.log('Servidor http corriendo en el puerto 8000');
 });
 
 app.get('/', function(req, res){
-    res.send('Hola, estas en la pagina principal');
+    res.send('Hola, estas en la pagina principal de Backend');
     console.log('Se recibio una peticion de tipo get');
 });
