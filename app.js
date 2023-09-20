2d8395458498ebd27248fd96882cde4699aa3839
MaximusYakuza <mmoralesalta@gmail.com>
2023-09-20 07:31:36 -0600

    Cambios

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+const styles 
+{
+    nombreDeClase: {
+        backgroundColor: "#FFF"
+    },
+    nombreDeClaseDos: {
+        color: "#000"
+    }
+}
